70dc848d847b4814d726ec254b2b63da59b8860a
ximengzhao <ximengzhao@microsoft.com>
2022-11-01 19:18:37 -0700

    commit vehicle.go

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,68 @@
 package main
 
-import (
+type vehicle interface {
+}
 
-)
+type car struct {
+	model       string
+	make        string
+	typeVehicle string
+}
 
+type truck struct {
+	model       string
+	make        string
+	typeVehicle string
+}
+
+type bike struct {
+	model string
+	make  string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json:"values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string/*
+	inventory = []vehicle{
+		bike{"FTR 1200", "Indian"},
+		bike{"Iron 1200", "Harley"},
+		car{"Sonata", "Hyundai", "Sedan"},
+		car{"SantaFe", "Hyundai", "SUV"},
+		car{"Civic", "Honda", "Hatchback"},
+		car{"A5", "Audi", "Coupe"},
+		car{"Mazda6", "Mazda", "Sedan"},
+		car{"CRV", "Honda", "SUV"},
+		car{"Camry", "Toyota", "Sedan"},
+		truck{"F-150", "Ford", "Truck"},
+		truck{"RAM1500", "Dodge", "Truck"}}
+*//*
+		inventory = []vehicle{
+			bike{"FTR 1200", "Indian"},
+			bike{"Iron 1200", "Harley"},
+			car{"Sonata", "Hyundai", "Sedan"},
+			car{"SantaFe", "Hyundai", "SUV"},
+			car{"Civic", "Honda", "Hatchback"},
+			car{"A5", "Audi", "Coupe"},
+			car{"Mazda6", "Mazda", "Sedan"},
+			car{"CRV", "Honda", "SUV"},
+			car{"Camry", "Toyota", "Sedan"},
+			truck{"F-150", "Ford", "Truck"},
+			truck{"RAM1500", "Dodge", "Truck"}}
+	*/ struct {
+	feedbackTotal    int
+	feedbackPostive  int
+	feedbackNegative int
+	feedbackNeutral  int
+}
+
+var vehicleResult map[string]feedbackResult
+
+var inventory []vehicle
 
 type rating float32
 
@@ -21,7 +76,6 @@ const (
 
 func init() {
 
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,15 +88,15 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
